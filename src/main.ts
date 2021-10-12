236c30dc94f20e4b3f6cb11e4ec11685c1bdbf85
RimlTempest <riml.slime@gmail.com>
2021-10-12 21:36:25 +0900

    add: nest.jsの導入

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app: NestFastifyApplication =
+    await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+    );
+  const config = new DocumentBuilder()
+    .setTitle('DotArt API')
+    .setDescription('DotArtのバックエンドAPI')
+    .setVersion('0.0.1')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(5000);
+  console.log('listen to http://localhost:5000');
+}
+bootstrap();
