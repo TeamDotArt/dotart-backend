8c246c1a668a964bd7c2893548ae18f79ed58f00
RimlTempest <riml.slime@gmail.com>
2021-10-14 10:51:14 +0900

    update: rootModuleにbasicpalletを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { UserpalletModule } from './userpallet/userpallet.module';
+import { BasicPalletModule } from './basic-pallet/basic-pallet.module';
 
 @Module({
-  imports: [UsersModule, UserpalletModule],
+  imports: [UsersModule, UserpalletModule, BasicPalletModule],
   controllers: [AppController],
   providers: [AppService],
 })
