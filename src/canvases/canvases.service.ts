b484ac0877161bbabd0211ee67295343f3b997a7
keykimu <kimuyuu0309@gmail.com>
2021-10-14 20:21:19 +0900

    add: canvasesの処理部を追加

---
+++ src/canvases/canvases.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './../prisma.service';
+import { Canvases, Prisma } from '@prisma/client';
+
+@Injectable()
+export class CanvasesService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.CanvasesCreateInput): Promise<Canvases> {
+    return this.prisma.canvases.create({ data });
+  }
+
+  async findAll(): Promise<Canvases[]> {
+    return this.prisma.canvases.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.canvases.findUnique({
+      where: { id: id },
+    });
+  }
+
+  async update(
+    id: number,
+    data: Prisma.CanvasesUpdateInput,
+  ): Promise<Canvases> {
+    return this.prisma.canvases.update({
+      where: { id: id },
+      data,
+    });
+  }
+
+  async remove(id: number): Promise<Canvases> {
+    return this.prisma.canvases.delete({
+      where: { id: id },
+    });
+  }
+}
