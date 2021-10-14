3daa77ad60e5255e3a57f62376e011d484d9765d
keykimu <kimuyuu0309@gmail.com>
2021-10-14 20:21:53 +0900

    add: canvasesのコントローラーを追加

---
+++ src/canvases/canvases.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CanvasesService } from './canvases.service';
+import { Canvases, Prisma } from '@prisma/client';
+
+@Controller('canvases')
+export class CanvasesController {
+  constructor(private readonly CanvasesService: CanvasesService) {}
+
+  @Post()
+  create(@Body() data: Prisma.CanvasesCreateInput): Promise<Canvases> {
+    return this.CanvasesService.create(data);
+  }
+
+  @Get()
+  async findAll(): Promise<Canvases[]> {
+    return this.CanvasesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Canvases> {
+    return this.CanvasesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() data: Prisma.CanvasesUpdateInput) {
+    return this.CanvasesService.update(+id, data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<Canvases> {
+    return this.CanvasesService.remove(+id);
+  }
+}
