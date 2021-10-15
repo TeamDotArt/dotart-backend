96b49d10d0e0512b89df8b481fabe87b8e1cf026
RimlTempest <riml.slime@gmail.com>
2021-10-15 23:58:40 +0900

    fix: prismaServiceを統一化したので修正

--- src/basic-pallet/basic-pallet.module.ts
+++ src/basic-pallet/basic-pallet.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BasicPalletService } from './basic-pallet.service';
 import { BasicPalletController } from './basic-pallet.controller';
-import { PrismaService } from './../prisma.service';
+import { PrismaService } from '../common/prisma.service';
 
 @Module({
   controllers: [BasicPalletController],
--- src/canvases/canvases.service.ts
+++ src/canvases/canvases.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from './../prisma.service';
+import { PrismaService } from '../common/prisma.service';
 import { Canvases, Prisma } from '@prisma/client';
 
 @Injectable()
