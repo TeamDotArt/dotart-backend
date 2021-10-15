4f09e12abf4c417a3cdc00cc68bdda2189fef0ad
RimlTempest <riml.slime@gmail.com>
2021-10-15 23:57:10 +0900

    add: 今後実装する自動生成されたファイル

---
+++ src/users/dto/decoded.dto.ts
@@ -0,0 +1,7 @@
+export class DecodedDto {
+  userId: string;
+  sub: number;
+  role: number;
+  iat: number;
+  exp: number;
+}
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,24 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @ApiProperty({ description: 'userId' })
+  @IsString()
+  @IsOptional()
+  userId: string;
+
+  @ApiProperty({ description: 'email' })
+  @IsEmail()
+  @IsOptional()
+  email: string;
+
+  @ApiProperty({ description: 'password' })
+  @IsString()
+  @IsOptional()
+  password: string;
+
+  @ApiProperty({ description: 'name' })
+  @IsString()
+  @IsOptional()
+  name: string;
+}
