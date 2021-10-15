6f9551447a9f2d658efe2620dfcd1a023a2596d4
RimlTempest <riml.slime@gmail.com>
2021-10-15 23:55:13 +0900

    add: 今後メール認証をする際に適用するコードの追加

--- src/common/sendgrid.service.ts
+++ src/common/sendgrid.service.ts
@@ -8,10 +8,15 @@ export const sendEmailToken = (email: string, emailToken: string) => {
   const msg = {
     to: email,
     from: 'dotart@gmail.com',
-    subject: 'Confirm email',
-    html: `http://localhost:5000/${emailToken}/confirm`,
+    subject: '認証メールです。',
+    html: `
+    DotArtの認証メールです。
+    以下のアクティベーションリンクにアクセスしメールアドレスの認証をおこなってください！
+    http://localhost:5000/${emailToken}/confirm
+    `,
   };
 
+  // TODO: メアド認証を実装する
   // sendgrid.send(msg);
   console.log('メール送信');
   console.log(msg);
