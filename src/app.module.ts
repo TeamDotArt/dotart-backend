423098714007a7c23a918a7308186428199b1604
RimlTempest <riml.slime@gmail.com>
2021-10-15 17:50:42 +0900

    add: モジュールに依存先を追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+// 依存Modules
 import { UsersModule } from './users/users.module';
 import { UserpalletModule } from './userpallet/userpallet.module';
 import { BasicPalletModule } from './basic-pallet/basic-pallet.module';
 import { CanvasesModule } from './canvases/canvases.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, UserpalletModule, BasicPalletModule, CanvasesModule],
+  imports: [
+    UsersModule,
+    UserpalletModule,
+    BasicPalletModule,
+    CanvasesModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      // envファイルを組み込むために使用
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
