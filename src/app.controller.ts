fa066dcce97ae9cb5c1389adf3b6c25865114e33
RimlTempest <riml.slime@gmail.com>
2021-10-15 17:49:56 +0900

    add: ログインやサインアップなどのRoutingを追加

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,65 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+
+type PasswordOmitUser = Omit<User, 'password'>;
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
+
+  @UseGuards(AuthGuard('local')) // passport-local戦略を付与する
+  @Post('login')
+  async login(@Request() req: { user: PasswordOmitUser }) {
+    console.log(req.user);
+    // LocalStrategy.validate()で認証して返した値がreq.userに入ってる
+    const user = req.user;
+
+    // JwtToken を返す
+    return this.authService.login(user);
+  }
+
+  @Post('logout')
+  logout(@Request() req) {
+    return this.authService.logout(req);
+  }
+
+  @Post('signup')
+  createUser(@Body() body: User) {
+    return this.authService.signup(body);
+  }
+
+  @Post(':emailToken/confirm')
+  confirmEmail(@Param('emailToken') emailToken: string) {
+    return this.authService.confirm(emailToken);
+  }
+
+  /**
+   * @description JWT認証を用いたサンプルAPI
+   */
+  @UseGuards(AuthGuard('jwt')) // passport-jwt戦略を付与する
+  @Get('profile')
+  getProfile(@Request() req: { user: PasswordOmitUser }) {
+    // JwtStrategy.validate()で認証して返した値がreq.userに入ってる
+    const user = req.user;
+
+    // 認証に成功したユーザーの情報を返す
+    return user;
+  }
 
   @Get()
   getHello(): string {
