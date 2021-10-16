6967414308a283fb7010a6b225646c1bdd582b95
RimlTempest <riml.slime@gmail.com>
2021-10-17 02:18:13 +0900

    doc: エンドポイントの記載、ドキュメントの設定を記載

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,19 +9,28 @@ import {
   Request,
   UseGuards,
 } from '@nestjs/common';
-import { FastifyRequest } from 'fastify';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { User } from '@prisma/client';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+// import { User } from '@prisma/client';
 // サービス
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 // DTO
 import { ConfirmedUserDto } from './auth/dto/confirmed-user.dto';
 import { LogInUserRequest, LogInUserResponse } from './auth/dto/login-user.dto';
-import { LogOutUserResponse } from './auth/dto/logout-user.dto';
-import { VerifyEmailDto } from './auth/dto/verify-email.dto';
+import {
+  LogOutUserRequest,
+  LogOutUserResponse,
+} from './auth/dto/logout-user.dto';
+import { VerifyEmailResponse } from './auth/dto/verify-email.dto';
 // ガード
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { User } from './users/entities/user.entity';
 
 type PasswordOmitUser = Omit<User, 'password'>;
 
@@ -35,7 +44,11 @@ export class AppController {
   ) {}
 
   @Post('login')
+  // Swagger定義
+  @ApiOperation({ summary: 'ログインを行う' })
   @ApiResponse({ status: HttpStatus.OK, type: LogInUserResponse })
+  @ApiBody({ type: LogInUserRequest, description: 'ログイン情報' })
+  // フックメソッド
   async login(@Body() req: LogInUserRequest): Promise<LogInUserResponse> {
     return this.authService.login(req);
   }
@@ -43,19 +56,34 @@ export class AppController {
   // TODO: RefreshTokenの実装を行いトークンの無効化も行う
   @UseGuards(JwtAuthGuard)
   @Post('logout')
+  // Swagger定義
+  @ApiOperation({ summary: 'ログアウトを行う' })
   @ApiResponse({ status: HttpStatus.OK, type: LogOutUserResponse })
-  logout(@Req() req: FastifyRequest): Promise<LogOutUserResponse> {
+  // フックメソッド
+  logout(@Req() req: LogOutUserRequest): Promise<LogOutUserResponse> {
     return this.authService.logout(req);
   }
 
   @Post('signup')
-  @ApiResponse({ status: HttpStatus.OK, type: VerifyEmailDto })
-  createUser(@Body() body: User): Promise<VerifyEmailDto> {
+  // Swagger定義
+  @ApiOperation({ summary: '新規アカウント作成を行う' })
+  @ApiResponse({ status: HttpStatus.OK, type: VerifyEmailResponse })
+  @ApiBody({ type: User, description: 'ユーザ情報' })
+  // フックメソッド
+  createUser(@Body() body: User): Promise<VerifyEmailResponse> {
     return this.authService.signup(body);
   }
 
   @Post(':emailToken/confirm')
+  // Swagger定義
+  @ApiOperation({ summary: 'メール認証を行う' })
   @ApiResponse({ status: HttpStatus.OK, type: ConfirmedUserDto })
+  @ApiParam({
+    name: 'emailToken',
+    description: 'emailToken情報',
+    type: String,
+  })
+  // フックメソッド
   confirmEmail(
     @Param('emailToken') emailToken: string,
   ): Promise<ConfirmedUserDto> {
@@ -67,6 +95,9 @@ export class AppController {
    */
   @UseGuards(JwtAuthGuard)
   @Get('profile')
+  // Swagger定義
+  @ApiOperation({ summary: 'ログインのガードが働いているかテスト' })
+  // フックメソッド
   getProfile(@Request() req: { user: PasswordOmitUser }) {
     const user = req.user;
 
