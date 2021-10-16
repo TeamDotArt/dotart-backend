479562315eaa8768692ad940c498f2dce765d69b
RimlTempest <riml.slime@gmail.com>
2021-10-16 17:47:47 +0900

    update: swagger定義とプレフィックスを追加

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import {
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const apiPrefix = 'api/v1';
   const app: NestFastifyApplication =
     await NestFactory.create<NestFastifyApplication>(
       AppModule,
@@ -15,6 +16,7 @@ async function bootstrap() {
         logger: ['error', 'warn', 'debug', 'verbose', 'log'],
       },
     );
+  app.setGlobalPrefix(apiPrefix);
 
   // Corsの有効化
   app.enableCors({
@@ -27,13 +29,16 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('DotArt API')
     .setDescription('DotArtのバックエンドAPI')
-    .setVersion('0.0.1')
-    .addTag('users')
+    .setVersion('0.0.3')
+    .setLicense(
+      'Apache-2.0 License',
+      'https://github.com/TeamDotArt/dotart-backend/blob/develop/LICENSE',
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(5000);
-  console.log('listen to http://localhost:5000');
+  console.log(`listen to http://localhost:5000/${apiPrefix}`);
 }
 bootstrap();
