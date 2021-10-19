92dc4053f9fd1872f5c85786d2925b149b9b4ec9
keykimu <kimuyuu0309@gmail.com>
2021-10-19 14:36:43 +0900

    add: create-user-pallet.dtoの追加

--- src/user-pallet/dto/create-user-pallet.dto.ts
+++ src/user-pallet/dto/create-user-pallet.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserPalletDto {}
+import { Exclude } from 'class-transformer';
+export class CreateUserPalletDto {
+  palletId: number;
+  name: string;
+  data: string;
+  @Exclude()
+  createdAt: string;
+}
