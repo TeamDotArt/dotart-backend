b60c264dab1abe5671eb8a74ab822e1d37e522ef
keykimu <kimuyuu0309@gmail.com>
2021-10-20 14:52:16 +0900

    fix: update-user-pallet.dtoのクラス名を変更

--- src/user-pallet/dto/update-user-pallet.dto.ts
+++ src/user-pallet/dto/update-user-pallet.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
-export class UpdateUserPalletDto {
+export class UpdateUserPalletRequest {
   @ApiProperty({ description: 'name' })
   @IsString()
   name: string;
