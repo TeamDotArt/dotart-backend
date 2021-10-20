2a120e9c91d782caf9fa6708a7e98f2183328c5e
keykimu <kimuyuu0309@gmail.com>
2021-10-20 14:08:02 +0900

    fix: controllerとdtoにタグ付け

--- src/user-pallet/dto/create-user-pallet.dto.ts
+++ src/user-pallet/dto/create-user-pallet.dto.ts
@@ -1,8 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 import { Exclude } from 'class-transformer';
 export class CreateUserPalletDto {
-  palletId: number;
+  @ApiProperty({ description: 'userId' })
+  @IsString()
+  userId: string;
+
+  @ApiProperty({ description: 'name' })
+  @IsString()
   name: string;
+
+  @ApiProperty({ description: 'data' })
+  @IsString()
   data: string;
+
   @Exclude()
   createdAt: string;
 }
--- src/user-pallet/user-pallet.controller.ts
+++ src/user-pallet/user-pallet.controller.ts
@@ -7,7 +7,7 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserPallet } from '@prisma/client';
 import { UserpalletService } from './user-pallet.service';
 import { CreateUserPalletDto } from './dto/create-user-pallet.dto';
@@ -20,21 +20,33 @@ export class UserPalletController {
   constructor(private readonly userPalletService: UserpalletService) {}
 
   @Post()
+  // Swagger定義
+  @ApiOperation({ summary: 'ユーザパレット生成' })
+  // フックメソッド
   create(@Body() createUserPalletDto: CreateUserPalletDto) {
     return;
   }
 
   @Get()
+  // Swagger定義
+  @ApiOperation({ summary: '全ユーザパレット検索' })
+  // フックメソッド
   findAll(): Promise<UserPallet[]> {
     return this.userPalletService.findAll();
   }
 
   @Get(':id')
+  // Swagger定義
+  @ApiOperation({ summary: 'IDから単一ユーザパレット検索' })
+  // フックメソッド
   findOne(@Param('id') id: string): Promise<UserPallet> {
     return this.userPalletService.findOne(+id);
   }
 
   @Patch(':id')
+  // Swagger定義
+  @ApiOperation({ summary: 'IDからユーザパレット更新' })
+  // フックメソッド
   update(
     @Param('id') id: string,
     @Body() updateUserPalletDto: UpdateUserPalletDto,
@@ -43,6 +55,9 @@ export class UserPalletController {
   }
 
   @Delete(':id')
+  // Swagger定義
+  @ApiOperation({ summary: 'IDからユーザパレット削除' })
+  // フックメソッド
   remove(@Param('id') id: string): Promise<UserPallet> {
     return this.userPalletService.remove(+id);
   }
