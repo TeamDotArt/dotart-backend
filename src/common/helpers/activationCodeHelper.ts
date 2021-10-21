cef8425129599f3aa479f0201e420ade4eb7f730
RimlTempest <riml.slime@gmail.com>
2021-10-21 15:15:35 +0900

    update: passwordトークン生成を実装

--- src/common/helpers/activationCodeHelper.ts
+++ src/common/helpers/activationCodeHelper.ts
@@ -1,3 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
+import { getHash } from './cipherHelper';
+
 export const generateEmailToken = (): string => {
   return Math.floor(10000000 + Math.random() * 90000000).toString();
+};
+
+export const generatePasswordToken = (userId: string): string => {
+  const id = uuidv4();
+  const date = Date.now().toString();
+  const hash = getHash(`${id} : ${date} : ${userId}`);
+  return hash;
 };
