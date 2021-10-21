5e2b87223667cd9fc38715434bf7a703c56a8f45
RimlTempest <riml.slime@gmail.com>
2021-10-21 15:59:19 +0900

    fix: /が入りバグるのでbase64に

--- src/common/helpers/activationCodeHelper.ts
+++ src/common/helpers/activationCodeHelper.ts
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import { getHash } from './cipherHelper';
+import { Buffer } from 'buffer';
 
 export const generateEmailToken = (): string => {
   return Math.floor(10000000 + Math.random() * 90000000).toString();
@@ -9,5 +10,6 @@ export const generatePasswordToken = (userId: string): string => {
   const id = uuidv4();
   const date = Date.now().toString();
   const hash = getHash(`${id} : ${date} : ${userId}`);
-  return hash;
+  const encodedData = Buffer.from(hash).toString('base64');
+  return encodedData;
 };
