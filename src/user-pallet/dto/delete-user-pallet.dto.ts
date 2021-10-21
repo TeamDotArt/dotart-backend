e6c3c85db6577984ed55f2dfce4663c1ba98a410
keykimu <kimuyuu0309@gmail.com>
2021-10-21 13:39:33 +0900

    add: userpalletを削除するdtoが抜けていたので追加

---
+++ src/user-pallet/dto/delete-user-pallet.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+import { Constants } from 'src/common/constants';
+import { UserPallet } from '../entities/user-pallet.entity';
+
+export class RemoveUserPalletResponse {
+  @ApiProperty({ description: Constants.VERIFY_STATUS })
+  @IsNumber()
+  status: number;
+
+  @ApiProperty({ description: Constants.VERIFY_MESSAGE })
+  @IsString()
+  message: string;
+
+  @ApiProperty({ description: Constants.PROPERTY_USER_ID })
+  @IsString()
+  palletId: UserPallet['palletId'];
+}
