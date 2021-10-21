eeca8ee40ca68db7999f17c5c396c60a70252531
keykimu <kimuyuu0309@gmail.com>
2021-10-21 13:47:45 +0900

    fix: 項目の追加とレスポンスを追加

--- src/user-pallet/dto/create-user-pallet.dto.ts
+++ src/user-pallet/dto/create-user-pallet.dto.ts
@@ -1,9 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
 import { Constants } from 'src/common/constants';
 import { UserPallet } from 'src/user-pallet/entities/user-pallet.entity';
 
 export class CreateUserPalletRequest {
+  @ApiProperty({ description: Constants.PROPERTY_USER_PALLET })
+  @IsString()
+  palletId: UserPallet['palletId'];
+
   @ApiProperty({ description: Constants.PROPERTY_USER_ID })
   @IsString()
   @IsNotEmpty({
@@ -18,4 +22,14 @@ export class CreateUserPalletRequest {
   @ApiProperty({ description: Constants.PROPERTY_PALLET_DATA })
   @IsString()
   data: UserPallet['data'];
+}
+
+export class CreateUserPalletResponse {
+  @ApiProperty({ description: Constants.VERIFY_STATUS })
+  @IsNumber()
+  status: number;
+
+  @ApiProperty({ description: Constants.VERIFY_MESSAGE })
+  @IsString()
+  message: string;
 }
