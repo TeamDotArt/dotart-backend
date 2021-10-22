968e29666b8c336123ec7787ce80a284a3685dcf
keykimu <kimuyuu0309@gmail.com>
2021-10-22 10:07:57 +0900

    add: 各dtoの定義(create,findAll,find,update,delete)

--- src/basic-pallet/dto/find-basic-pallet.dto.ts
+++ src/basic-pallet/dto/find-basic-pallet.dto.ts
@@ -1,32 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNumber } from 'class-validator';
+import { IsString, IsNumber, IsNotEmpty, IsDate } from 'class-validator';
 import { Constants } from 'src/common/constants';
 import { BasicPallet } from 'src/basic-pallet/entities/basic-pallet.entity';
 
-export class CreateBasicPalletRequest {
+export class FindBasicPalletResponse {
   @ApiProperty({ description: Constants.PROPERTY_BASIC_PALLET_ID })
   @IsNumber()
+  @IsNotEmpty()
   palletId: BasicPallet['palletId'];
 
   @ApiProperty({ description: Constants.PROPERTY_BASIC_PALLET_NAME })
   @IsString()
+  @IsNotEmpty()
   name: BasicPallet['name'];
 
   @ApiProperty({ description: Constants.PROPERTY_BASIC_PALLET_DESCRIPTION })
   @IsString()
+  @IsNotEmpty()
   description: BasicPallet['description'];
 
   @ApiProperty({ description: Constants.PROPERTY_BASIC_PALLET_DATA })
   @IsString()
+  @IsNotEmpty()
   data: BasicPallet['data'];
-}
 
-export class CreateBasicPalletResponse {
-  @ApiProperty({ description: Constants.VERIFY_STATUS })
-  @IsNumber()
-  status: number;
-
-  @ApiProperty({ description: Constants.VERIFY_MESSAGE })
-  @IsString()
-  message: string;
+  @ApiProperty({ description: Constants.PROPERTY_CREATED_AT })
+  @IsDate()
+  createdAt: BasicPallet['createdAt'];
 }
