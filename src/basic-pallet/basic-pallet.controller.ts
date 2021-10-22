e5021db7b98e54da050522f71aaaacc270a71c0a
keykimu <kimuyuu0309@gmail.com>
2021-10-22 10:12:46 +0900

    fix: Swaggerタグ付け、dto適用

--- src/basic-pallet/basic-pallet.controller.ts
+++ src/basic-pallet/basic-pallet.controller.ts
@@ -1,20 +1,34 @@
-import { Prisma, BasicPallet } from '@prisma/client';
 import {
+  Body,
   Controller,
   Get,
-  Post,
-  Body,
-  Patch,
+  HttpStatus,
   Param,
+  Post,
+  Req,
+  Patch,
   Delete,
 } from '@nestjs/common';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
-
 import { BasicPalletService } from './basic-pallet.service';
-
-// Prismaの型定義でよさそう
-// import { CreateBasicPalletDto } from './dto/create-basic-pallet.dto';
-// import { UpdateBasicPalletDto } from './dto/update-basic-pallet.dto';
+import {
+  ApiOperation,
+  ApiTags,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+} from '@nestjs/swagger';
+import {
+  CreateBasicPalletRequest,
+  CreateBasicPalletResponse,
+} from './dto/create-basic-pallet.dto';
+import { FindAllBasicPalletResponse } from './dto/findAll-basic-pallet.dto';
+import { FindBasicPalletResponse } from './dto/find-basic-pallet.dto';
+import {
+  UpdateBasicPalletRequest,
+  UpdateBasicPalletResponse,
+} from './dto/update-basic-pallet.dto';
+import { FastifyRequest } from 'fastify';
+import { RemoveBasicPalletResponse } from './dto/delete-basic-pallet.dto';
 
 // TODO: ApiResponseを記載する
 @ApiTags('basic-pallet')
@@ -23,28 +37,63 @@ export class BasicPalletController {
   constructor(private readonly basicPalletService: BasicPalletService) {}
 
   @Post()
-  // @ApiResponse({ status: 200, type: dto })
-  create(@Body() data: Prisma.BasicPalletCreateInput): Promise<BasicPallet> {
+  // Swagger定義
+  @ApiOperation({ summary: 'ユーザパレット生成' })
+  @ApiResponse({ status: HttpStatus.OK, type: CreateBasicPalletResponse })
+  @ApiBody({ type: CreateBasicPalletRequest, description: '生成データ' })
+  // フックメソッド
+  async create(
+    @Body() data: CreateBasicPalletRequest,
+  ): Promise<CreateBasicPalletResponse> {
     return this.basicPalletService.create(data);
   }
 
   @Get()
-  findAll(): Promise<BasicPallet[]> {
+  // Swagger定義
+  @ApiOperation({ summary: '全ベーシックパレット検索' })
+  @ApiResponse({ status: HttpStatus.OK, type: FindAllBasicPalletResponse })
+  // フックメソッド
+  async findAll(): Promise<FindAllBasicPalletResponse[]> {
     return this.basicPalletService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string): Promise<BasicPallet> {
-    return this.basicPalletService.findOne(+id);
+  @Get(':palletId')
+  // Swagger定義
+  @ApiOperation({ summary: 'palletIdから単一ベーシックパレット検索' })
+  @ApiResponse({ status: HttpStatus.OK, type: FindBasicPalletResponse })
+  @ApiParam({
+    name: 'palletId',
+    description: 'ベーシックパレットのId',
+    type: String,
+  })
+  // フックメソッド
+  async findByBasicPalletId(
+    @Param('palletId') palletId: number,
+  ): Promise<FindBasicPalletResponse> {
+    return this.basicPalletService.findByBasicPalletId(palletId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() data: Prisma.BasicPalletUpdateInput) {
-    return this.basicPalletService.update(+id, data);
+  @Patch()
+  // Swagger定義
+  @ApiOperation({ summary: 'ベーシックパレット更新' })
+  @ApiResponse({ status: HttpStatus.OK, type: UpdateBasicPalletResponse })
+  @ApiBody({ type: UpdateBasicPalletRequest, description: '更新データ' })
+  // フックメソッド
+  async updateProfileData(
+    @Req() req: FastifyRequest,
+    @Body() data: UpdateBasicPalletRequest,
+  ): Promise<UpdateBasicPalletResponse> {
+    return this.basicPalletService.updateBasicPalletData(req, data);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): Promise<BasicPallet> {
-    return this.basicPalletService.remove(+id);
+  // Swagger定義
+  @ApiOperation({ summary: 'ベーシックパレット削除' })
+  @ApiResponse({ status: HttpStatus.OK, type: RemoveBasicPalletResponse })
+  // フックメソッド
+  async removeBasicPalletData(
+    @Req() req: FastifyRequest,
+  ): Promise<RemoveBasicPalletResponse> {
+    return this.basicPalletService.removeBasicPalletData(req);
   }
 }
