858910aa46aa851d0dc8f3e7754b11f06ce476b3
keykimu <kimuyuu0309@gmail.com>
2021-10-25 15:11:41 +0900

    fix: basicpallet.servisの修正
    非同期処理できるようawaitの追加
    名前検索用の関数追加
    引数をidではなくpalletIdに変更

--- src/basic-pallet/basic-pallet.service.ts
+++ src/basic-pallet/basic-pallet.service.ts
@@ -7,13 +7,10 @@ import {
 } from './dto/create-basic-pallet.dto';
 import { FindBasicPalletResponse } from './dto/find-basic-pallet.dto';
 import { FindAllBasicPalletResponse } from './dto/findAll-basic-pallet.dto';
-import { FastifyRequest } from 'fastify';
-import jwt_decode from 'jwt-decode';
 import {
   UpdateBasicPalletRequest,
   UpdateBasicPalletResponse,
 } from './dto/update-basic-pallet.dto';
-import { DecodedDto } from 'src/auth/dto/decoded.dto';
 import { RemoveBasicPalletResponse } from './dto/delete-basic-pallet.dto';
 
 @Injectable()
@@ -23,25 +20,30 @@ export class BasicPalletService {
   async create(
     data: CreateBasicPalletRequest,
   ): Promise<CreateBasicPalletResponse> {
-    this.prisma.basicPallet.create({ data });
-    return {
+    await this.prisma.basicPallet.create({ data: data });
+
+    const ret: CreateBasicPalletResponse = {
       status: 201,
       message: 'ベーシックパレットを生成しました。',
     };
+
+    return ret;
   }
 
   async findAll(): Promise<FindAllBasicPalletResponse[]> {
     return this.prisma.basicPallet.findMany();
   }
 
+  /*
   async findOne(id: number) {
     return this.prisma.basicPallet.findUnique({
       where: { id: id },
     });
   }
+*/
 
   async findByBasicPalletId(
-    palletId: number,
+    palletId: string,
   ): Promise<FindBasicPalletResponse> {
     if (!palletId) {
       throw new NotFoundException('palletIdが存在しません。');
@@ -50,7 +52,7 @@ export class BasicPalletService {
       where: { palletId: palletId },
     });
     const ret: FindBasicPalletResponse = {
-      palletId: basicpallet.palletId,
+      palletId: palletId,
       name: basicpallet.name,
       description: basicpallet.description,
       data: basicpallet.data,
@@ -59,45 +61,65 @@ export class BasicPalletService {
     return ret;
   }
 
+  async findByName(name: string): Promise<FindBasicPalletResponse> {
+    if (!name) {
+      throw new NotFoundException('palletnameが存在しません。');
+    }
+    const basicpallet = await this.prisma.basicPallet.findUnique({
+      where: { name: name },
+    });
+    const ret: FindBasicPalletResponse = {
+      palletId: basicpallet.palletId,
+      name: name,
+      description: basicpallet.description,
+      data: basicpallet.data,
+      createdAt: basicpallet.createdAt,
+    };
+    return ret;
+  }
+
   async updateBasicPalletData(
-    req: FastifyRequest,
+    palletId: string,
     data: UpdateBasicPalletRequest,
   ): Promise<UpdateBasicPalletResponse> {
-    const decoded: DecodedDto = jwt_decode(req.headers.authorization);
     const basicpallet: BasicPallet = await this.prisma.basicPallet.findUnique({
-      where: { id: decoded.id },
+      where: { palletId: palletId },
     });
     if (!basicpallet) {
       throw new NotFoundException('ベーシックパレットが存在しません。');
     }
-    this.prisma.basicPallet.update({
-      where: { id: basicpallet.id },
-      data,
+    await this.prisma.basicPallet.update({
+      where: { palletId: palletId },
+      data: {
+        name: data.name,
+        description: data.description,
+        data: data.data,
+      },
     });
-    return {
+    const ret: UpdateBasicPalletResponse = {
       status: 201,
       message: '更新しました。',
     };
+    return ret;
   }
 
   async removeBasicPalletData(
-    req: FastifyRequest,
+    palletId: string,
   ): Promise<RemoveBasicPalletResponse> {
-    const decoded: DecodedDto = jwt_decode(req.headers.authorization);
     const basicpallet: BasicPallet = await this.prisma.basicPallet.findUnique({
-      where: { id: decoded.id },
+      where: { palletId: palletId },
     });
     if (!basicpallet) {
       throw new NotFoundException('ベーシックパレットが存在しません。');
     }
-    const palletId = basicpallet.palletId;
-    this.prisma.basicPallet.delete({
-      where: { id: basicpallet.id },
+    await this.prisma.basicPallet.delete({
+      where: { palletId: palletId },
     });
-    return {
+    const ret: RemoveBasicPalletResponse = {
       status: 201,
       message: 'ベーシックパレットを削除しました。',
       palletId: palletId,
     };
+    return ret;
   }
 }
