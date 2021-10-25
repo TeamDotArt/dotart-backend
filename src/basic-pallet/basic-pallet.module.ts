dcb159f818a625e158619edf9c261104a2d48782
keykimu <kimuyuu0309@gmail.com>
2021-10-25 15:01:46 +0900

    fix: basicpallet.moduleにUsersServisを追加
    ガード使用するため

--- src/basic-pallet/basic-pallet.module.ts
+++ src/basic-pallet/basic-pallet.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BasicPalletService } from './basic-pallet.service';
 import { BasicPalletController } from './basic-pallet.controller';
 import { PrismaService } from '../common/prisma.service';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [BasicPalletController],
-  providers: [BasicPalletService, PrismaService],
+  providers: [BasicPalletService, PrismaService, UsersService],
 })
 export class BasicPalletModule {}
