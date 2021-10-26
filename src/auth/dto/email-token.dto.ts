434ae3dfbe335b66ccb27dd532cbeaca883a0a0f
RimlTempest <riml.slime@gmail.com>
2021-10-26 15:29:30 +0900

    update: Dtoを厳密に整備

---
+++ src/auth/dto/email-token.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Constants } from 'src/common/constants';
+
+export class EmailTokenParam {
+  @ApiProperty({ description: Constants.PROPERTY_EMAIL_TOKEN })
+  @IsString()
+  @IsNotEmpty({
+    message: Constants.IS_NOT_EMPTY_EMAIL_TOKEN,
+  })
+  emailToken: string;
+}
