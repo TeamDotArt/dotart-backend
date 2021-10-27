03b10918d817ec193a5b2370a82b4dd1656cac56
keykimu <kimuyuu0309@gmail.com>
2021-10-27 16:12:33 +0900

    fix: guard用にUsersServiceの追加

--- src/canvases/canvases.module.ts
+++ src/canvases/canvases.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CanvasesService } from './canvases.service';
 import { CanvasesController } from './canvases.controller';
 import { PrismaService } from '../common/prisma.service';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [CanvasesController],
-  providers: [CanvasesService, PrismaService],
+  providers: [CanvasesService, PrismaService, UsersService],
 })
 export class CanvasesModule {}
