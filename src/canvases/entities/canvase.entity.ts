e320d704a40cc85651ad0a60eae8c0cb64db9dd1
keykimu <kimuyuu0309@gmail.com>
2021-10-27 16:11:08 +0900

    fix: canvasのentity定義

--- src/canvases/entities/canvase.entity.ts
+++ src/canvases/entities/canvase.entity.ts
@@ -1,1 +1,68 @@
-export class Canvase {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Canvases as PrismaCanvas, User } from '@prisma/client';
+import { Exclude } from 'class-transformer';
+import {
+  IsNumber,
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsDate,
+} from 'class-validator';
+import { Constants } from 'src/common/constants';
+
+export class Canvas {
+  @ApiProperty({ description: Constants.PROPERTY_ID })
+  @IsNumber()
+  @IsNotEmpty()
+  @Exclude()
+  id: PrismaCanvas['id'];
+
+  @ApiProperty({ description: Constants.PROPERTY_CANVAS_ID })
+  @IsString()
+  @IsNotEmpty()
+  @Exclude()
+  canvasId: PrismaCanvas['canvasId'];
+
+  @ApiProperty({ description: Constants.PROPERTY_USER_ID })
+  @IsString()
+  @IsNotEmpty()
+  @Exclude()
+  userId: PrismaCanvas['userId'];
+
+  @ApiProperty({ description: Constants.PROPERTY_CANVAS_NAME })
+  @IsString()
+  @IsNotEmpty()
+  canvasName: PrismaCanvas['canvasName'];
+
+  @ApiProperty({ description: Constants.PROPERTY_CANVAS_RANGE })
+  @IsNumber()
+  @IsNotEmpty()
+  canvasRange: PrismaCanvas['canvasRange'];
+
+  @ApiProperty({ description: Constants.PROPERTY_PALLET })
+  @IsString()
+  @IsNotEmpty()
+  pallet: PrismaCanvas['pallet'];
+
+  @ApiProperty({ description: Constants.PROPERTY_INDEX_DATA })
+  @IsString()
+  @IsNotEmpty()
+  indexData: PrismaCanvas['indexData'];
+
+  //子要素系
+  @ApiProperty({ description: Constants.PROPERTY_NAME })
+  @IsString()
+  @IsOptional()
+  user?: User[];
+
+  //時間系
+  @ApiProperty({ description: Constants.PROPERTY_CREATED_AT })
+  @IsDate()
+  @Exclude()
+  createdAt: PrismaCanvas['createdAt'];
+
+  @ApiProperty({ description: Constants.PROPERTY_UPDATED_AT })
+  @IsDate()
+  @Exclude()
+  updatedAt: PrismaCanvas['updatedAt'];
+}
