69a690deac4b9a48c05d58baa8f4612f7422e49f
RimlTempest <riml.slime@gmail.com>
2021-10-27 16:09:24 +0900

    update: Tokenサービスの実装

--- src/token/entities/token.entity.ts
+++ src/token/entities/token.entity.ts
@@ -1,2 +1,36 @@
-export class Token {}
-//TODO: Entityを整える
+import { ApiProperty } from '@nestjs/swagger';
+import { Token as PrismaToken } from '@prisma/client';
+import { Exclude } from 'class-transformer';
+import { IsNumber, IsString, IsNotEmpty } from 'class-validator';
+import { Constants } from 'src/common/constants';
+
+export class Token {
+  @ApiProperty({ description: Constants.PROPERTY_ID })
+  @IsNumber()
+  @IsNotEmpty()
+  @Exclude()
+  id: PrismaToken['id'];
+
+  @ApiProperty({ description: Constants.PROPERTY_USER_ID })
+  @IsString()
+  @IsNotEmpty()
+  @Exclude()
+  userId: PrismaToken['userId'];
+
+  @ApiProperty({ description: Constants.PROPERTY_TOKEN })
+  @IsString()
+  token: PrismaToken['token'];
+
+  @ApiProperty({ description: Constants.PROPERTY_REFRESH_TOKEN })
+  @IsString()
+  refreshToken: PrismaToken['refreshToken'];
+
+  @ApiProperty({ description: Constants.PROPERTY_EMAIL_TOKEN })
+  @IsString()
+  //TODO: Prismaを整える
+  emailToken: PrismaToken['emailToken'];
+
+  @ApiProperty({ description: Constants.PROPERTY_PASSWORD_TOKEN })
+  @IsString()
+  passwordToken: PrismaToken['passwordToken'];
+}
--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -3,17 +3,96 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/common/prisma.service';
 import { CreateTokenDto } from './dto/create-token.dto';
 import { UpdateTokenDto } from './dto/update-token.dto';
+import { PayloadDto } from 'src/auth/dto/payload.dto';
+import { JwtService } from '@nestjs/jwt';
+import { generateEmailToken } from 'src/common/helpers/activationCodeHelper';
 
 // TODO: Token操作をこちらで行うようにする
 @Injectable()
 export class TokenService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
-  async findAll(): Promise<Token[]> {
-    return this.prisma.token.findMany();
+  async getTokenByUserId(userId: string): Promise<string> {
+    const findToken = await this.prisma.token.findUnique({
+      where: {
+        userId: userId,
+      },
+    });
+    return findToken.token;
   }
 
-  async remove(userId: string): Promise<Token> {
+  async getRefreshTokenByUserId(userId: string): Promise<string> {
+    const findToken = await this.prisma.token.findUnique({
+      where: {
+        userId: userId,
+      },
+    });
+    return findToken.refreshToken;
+  }
+
+  async getEmailTokenByUserId(userId: string): Promise<string> {
+    const findToken = await this.prisma.token.findUnique({
+      where: {
+        userId: userId,
+      },
+    });
+    return findToken.emailToken;
+  }
+
+  async getUserIdByEmailToken(emailToken: string): Promise<string> {
+    const findToken = await this.prisma.token.findUnique({
+      where: {
+        emailToken: emailToken,
+      },
+    });
+    return findToken.userId;
+  }
+
+  async getUserIdByPasswordToken(passwordToken: string): Promise<string> {
+    const findToken = await this.prisma.token.findUnique({
+      where: {
+        passwordToken: passwordToken,
+      },
+    });
+    return findToken.userId;
+  }
+
+  async getPasswordTokenByUserId(userId: string): Promise<string> {
+    const findToken = await this.prisma.token.findUnique({
+      where: {
+        userId: userId,
+      },
+    });
+    return findToken.passwordToken;
+  }
+
+  async createToken(payload: PayloadDto): Promise<string> {
+    const accessToken = this.jwtService.sign(payload);
+    await this.prisma.token.upsert({
+      where: { userId: payload.userId },
+      update: {
+        token: accessToken,
+      },
+      create: {
+        userId: payload.userId,
+        token: accessToken,
+      },
+    });
+    return accessToken;
+  }
+
+  async createEmailToken(userId: string): Promise<string> {
+    const emailToken = generateEmailToken();
+    const token = await this.prisma.token.create({
+      data: {
+        userId: userId,
+        emailToken: emailToken,
+      },
+    });
+    return token.emailToken;
+  }
+
+  async removeTokenByUserId(userId: string): Promise<Token> {
     return this.prisma.token.delete({
       where: { userId: userId },
     });
