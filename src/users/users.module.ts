96470723448b631e20e398d89d1d52724418a24b
RimlTempest <riml.slime@gmail.com>
2021-10-29 14:43:58 +0900

    update: DIの修正

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/common/prisma.service';
+import { TokenService } from 'src/token/token.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, TokenService],
 })
 export class UsersModule {}
