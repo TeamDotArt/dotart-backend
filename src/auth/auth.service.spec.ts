1945e7dbc7a5320996b05244e758c46b15351b32
keykimu <kimuyuu0309@gmail.com>
2021-11-11 01:15:46 +0900

    fix: auth.service.specの修正

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TokenService } from '../token/token.service';
+import { UsersService } from '../users/users.service';
+import { PrismaService } from '../common/prisma.service';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LocalStrategy } from './strategy/local.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        // JWTを使うための設定をしている
+        JwtModule.registerAsync({
+          // ここでimportしないとconfigServiceが参照できない
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => {
+            return {
+              secret: configService.get<string>('JWT_SECRET_KEY'),
+              signOptions: {
+                expiresIn: '1200s',
+              },
+            };
+          },
+          inject: [ConfigService],
+        }),
+      ],
+      providers: [
+        AuthService,
+        PrismaService,
+        UsersService,
+        TokenService,
+        LocalStrategy,
+        JwtStrategy,
+        ConfigService,
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
