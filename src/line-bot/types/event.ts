85275f3f15026d7edf1ab196bf2898f6972993b0
RimlTempest <riml.slime@gmail.com>
2021-11-11 16:00:40 +0900

    add: type定数を定義

---
+++ src/line-bot/types/event.ts
@@ -0,0 +1,15 @@
+export class EventTypes {
+  public static readonly MESSAGE = 'message';
+  public static readonly UN_SEND = 'unsend';
+  public static readonly FOLLOW = 'follow';
+  public static readonly UN_FOLLOW = 'unfollow';
+  public static readonly JOIN = 'join';
+  public static readonly LEAVE = 'leave';
+  public static readonly MEMBER_JOINED = 'memberJoined';
+  public static readonly MEMBER_LEFT = 'memberLeft';
+  public static readonly POSTBACK = 'postback';
+  public static readonly VIDEOPLAY_COMPLETE = 'videoPlayComplete';
+  public static readonly BEACON = 'beacon';
+  public static readonly ACCOUNT_LINK = 'accountLink';
+  public static readonly THINGS = 'things';
+}
---
+++ src/line-bot/types/message.ts
@@ -0,0 +1,9 @@
+export class MessageTypes {
+  public static readonly TEXT = 'text';
+  public static readonly IMAGE = 'image';
+  public static readonly VIDEO = 'video';
+  public static readonly AUDIO = 'audio';
+  public static readonly LOCATION = 'location';
+  public static readonly STICKER = 'sticker';
+  public static readonly FILE = 'file';
+}
