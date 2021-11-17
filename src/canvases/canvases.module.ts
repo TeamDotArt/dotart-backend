100ec4fc770d6e2496160054c956e6f6114f4572
keykimu <kimuyuu0309@gmail.com>
2021-11-17 16:53:26 +0900

    fix: moduleのパスの修正

--- src/canvases/canvases.module.ts
+++ src/canvases/canvases.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { CanvasesService } from './canvases.service';
 import { CanvasesController } from './canvases.controller';
 import { PrismaService } from '../common/prisma.service';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
 import { TokenService } from '../token/token.service';
 
 @Module({
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaService } from 'src/common/prisma.service';
-import { TokenService } from 'src/token/token.service';
+import { PrismaService } from '../common/prisma.service';
+import { TokenService } from '../token/token.service';
 
 @Module({
   controllers: [UsersController],
